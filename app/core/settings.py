f99efc79feadcb27a50ea4f3e1ce15162520a918
Eldar Klyuev <e.s.klyuev@gmail.com>
2023-12-03 23:07:07 +0300

    Initial client

--- app/core/settings.py
+++ app/core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'vote',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vote',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'AUTOCOMMIT': True,
     }
 }
 
--- app/vote/models.py
+++ app/vote/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=255, blank=False, unique=True)
+    username = models.CharField(max_length=255, blank=False, unique=True)
+    password = models.CharField(max_length=255, blank=False)
+
+    ROLE_CHOISES = [
+        ('Admin', 'Admin'),
+        ('User', 'User')
+    ]
+    role_sys = models.CharField(max_length=20,
+                                choices=ROLE_CHOISES,
+                                default=ROLE_CHOISES[1][1],
+                                null=True)
+    
+    is_enable = models.BooleanField(default=False)
+    is_verify = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.username
+    
+    class Meta:
+        db_table = 'public"."users'
+
+
+class Voting(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    time_start = models.DateTimeField()
+    time_end = models.DateTimeField()
+
+    class Meta:
+        db_table = 'public"."voting'
+
+
+# class VotingChoises
